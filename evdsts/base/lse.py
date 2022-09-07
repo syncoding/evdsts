a7c5fc28895c03fe026bdd398582c984146f8311
Synertic <synertic@gmail.com>
2022-09-07 19:44:24 +0300

    LSModelling

--- evdsts/base/lse.py
+++
@@ -1,7 +0,0 @@
-"""evdsts Least Squares Estimator Class"""
-
-__author__ = "Burak CELIK"
-__copyright__ = "Copyright (c) 2022 Burak CELIK"
-__license__ = "MIT"
-__version__ = "1.0rc4"
-__internal__ = "0.0.1"
--- evdsts/configuration/types.py
+++ evdsts/configuration/types.py
@@ -10,10 +10,13 @@ __internal__ = "0.0.1"
 from datetime import datetime
 from typing import Any, Union, Dict, List
 
-from pandas import Timestamp
+from pandas import Timestamp, DataFrame, Series
 
 #* Define a JSON type for annotating raw returns.
 JSONType = Union[str, int, float, bool, None, List[Any], Dict[str, Any]]
 
 #* Define a DateLike type for date/time operations
 DateLike = Union[None, str, datetime, Timestamp]
+
+#* Define a FrameLike for data which can be represented as a DataFrame
+FrameLike = Union[Dict, Series, DataFrame]
